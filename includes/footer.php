2dbaf43d94f47ee605d687e5aa1040ae3f442382
Kim Cyrus Tinio <kimcyrustinio@gmail.com>
2025-12-04 16:55:48 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+    <footer>&copy; <?php echo date('2025')?></footer>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sugar Daddy</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>Sugar Daddy</h1>
+    <nav>
+      <a href="index.php">Home</a> | 
+      <a href="product.php">Candy</a> | 
+      <a href="about.php">About</a> | 
+      <a href="contact.php">Contact</a>
+    </nav>
